bad85366f21d722cdef773559a3d87dc36ae1b24
Khinshan Khan <kkhan@stuy.edu>
2018-04-11 02:29:33 -0400

    the new poly arg needs to be accounted for

--- main.py
+++ main.py
@@ -15,6 +15,6 @@ transform = new_matrix()
 # print_matrix( make_hermite() )
 # print
 
-parse_file( 'script', edges, transform, screen, color )
+parse_file( 'script', poly, edges, transform, screen, color )
 #needs special save method and stuff -- for windows
 #parse_file( 'jimmyneutronlogo', poly, edges, transform, screen, color )
