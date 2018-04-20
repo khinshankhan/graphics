6372e5cba462c21dad6009cee6a5a043492aaae7
Khinshan Khan <kkhan@stuy.edu>
2018-04-20 15:36:06 -0400

    period ended

--- display.py
+++ display.py
@@ -30,7 +30,8 @@ def new_zbuffer( width = XRES, height = YRES ):
 def plot( screen, zbuffer, color, x, y, z ):
     newy = YRES - 1 - y
     if ( x >= 0 and x < XRES and newy >= 0 and newy < YRES):
-        screen[newy][x] = color[:]
+        if(z > zbuffer[newy][x]):
+            screen[newy][x] = color[:]
 
 def clear_screen( screen ):
     for y in range( len(screen) ):
