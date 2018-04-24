017dcf8f6d89e6a66a0bc9c8a198bc17493fbf6a
kkhan01 <khanshan501@gmail.com>
2018-04-24 06:45:44 -0400

    Update display.py

--- display.py
+++ display.py
@@ -29,9 +29,9 @@ def new_zbuffer( width = XRES, height = YRES ):
 
 def plot( screen, zbuffer, color, x, y, z ):
     newy = YRES - 1 - y
-    if ( x >= 0 and x < XRES and newy >= 0 and newy < YRES):
-        if(z > zbuffer[newy][x]):
-            screen[newy][x] = color[:]
+    if ( x >= 0 and x < XRES and newy >= 0 and newy < YRES and zb[newy][x] < z):
+        screen[newy][x] = color[:]
+        zb[newy][x] = z
 
 def clear_screen( screen ):
     for y in range( len(screen) ):
