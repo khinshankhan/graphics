931b95b88b288459a0c133743950f4a6288e95f2
Khinshan Khan <kkhan@stuy.edu>
2018-05-14 15:10:10 -0400

    basic skeleton

---
+++ script.py
@@ -0,0 +1,48 @@
+import mdl
+from display import *
+from matrix import *
+from draw import *
+
+def run(filename):
+    """
+    This function runs an mdl script
+    """
+    view = [0,
+            0,
+            1];
+    ambient = [50,
+               50,
+               50]
+    light = [[0.5,
+              0.75,
+              1],
+             [0,
+              255,
+              255]]
+    areflect = [0.1,
+                0.1,
+                0.1]
+    dreflect = [0.5,
+                0.5,
+                0.5]
+    sreflect = [0.5,
+                0.5,
+                0.5]
+
+    color = [0, 0, 0]
+    tmp = new_matrix()
+    ident( tmp )
+
+    stack = [ [x[:] for x in tmp] ]
+    screen = new_screen()
+    zbuffer = new_zbuffer()
+    tmp = []
+    step_3d = 20
+
+    p = mdl.parseFile(filename)
+
+    if p:
+        (commands, symbols) = p
+    else:
+        print "Parsing failed."
+        return
