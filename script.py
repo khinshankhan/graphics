7e401c9bfb4622f3aa3bd6b99f07f9d51102544c
Khinshan Khan <kkhan@stuy.edu>
2018-05-16 23:55:27 -0400

    done. need a custom, unique scriptor

--- script.py
+++ script.py
@@ -7,43 +7,74 @@ def run(filename):
     """
     This function runs an mdl script
     """
+    global view
     view = [0,
             0,
             1];
+    global ambient
     ambient = [50,
                50,
                50]
+    global light
     light = [[0.5,
               0.75,
               1],
              [0,
               255,
               255]]
+    global areflect
     areflect = [0.1,
                 0.1,
                 0.1]
+    global dreflect
     dreflect = [0.5,
                 0.5,
                 0.5]
+    global sreflect
     sreflect = [0.5,
                 0.5,
                 0.5]
 
+    global color
     color = [0, 0, 0]
     tmp = new_matrix()
     ident( tmp )
 
+    global stack
     stack = [ [x[:] for x in tmp] ]
+    global screen
     screen = new_screen()
+    global zbuffer
     zbuffer = new_zbuffer()
+    global temp
     tmp = []
+    global step_3d
     step_3d = 20
     #extra variables, but they help me
+    global polygons
     polygons = []
+    global edges
     edges = []
 
     #executes the mdl commands
     def execute(line, args):
+        #variables
+        global view
+        global ambient
+        global light
+        global areflect
+        global dreflect
+        global sreflect
+        global color
+        global stack
+        global screen
+        global zbuffer
+        global tmp
+        global step_3d
+        #extra variables, but they help me
+        global polygons
+        global edges
+        
         #loop!    
         if line == 'push':
             stack.append( [x[:] for x in stack[-1]] )
@@ -109,11 +140,11 @@ def run(filename):
     if p:
         #print p
         (commands, symbols) = p
-        print '###################'
-        print commands
+        #print '###################'
+        #print commands
         #print len(commands)
         #print len(commands[1])
-        print '###################'
+        #print '###################'
         #print symbols
                 
         for i in range(0, len(commands)):
@@ -121,7 +152,7 @@ def run(filename):
             args = []
             for j in range(1, len(commands[i])):
                 args.append(commands[i][j])
-            print (command, args)
+            #print (command, args)
             execute(command, args)
     else:
         print "Parsing failed."
